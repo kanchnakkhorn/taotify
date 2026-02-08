a89ee99dfeb49d090d5cb7da267a75f533da1fd5
Kanchnak Khorn <kanchnakkhorn@gmail.com>
2026-02-08 16:47:20 +0700

    chore: added compose, retrofit, okhttp, navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,11 @@ dependencies {
   androidTestImplementation(libs.androidx.compose.ui.test.junit4)
   debugImplementation(libs.androidx.compose.ui.tooling)
   debugImplementation(libs.androidx.compose.ui.test.manifest)
+  implementation("androidx.navigation:navigation-compose:2.8.5")
+
+  // Retrofit & OkHttp
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+  implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
+  implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
 }
